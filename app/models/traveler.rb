6e9f42cafca20e8d4f2a77e8ece82571bee4b6ee
Sushil Kumar <sushilkumar@chicagobooth.edu>
2017-06-12 17:31:49 -0500

    Edit3

--- app/models/traveler.rb
+++ app/models/traveler.rb
@@ -1,9 +1,12 @@
 class Traveler < ApplicationRecord
   belongs_to :user
 
+
+
   validates :acceptable_weight_lbs, presence: { message: "must be present" }
   validates :expected_fee , presence: { message: "must be present" }
 
-
+  belongs_to :origin, :class_name => "City"
+  belongs_to :destination, :class_name => "City"
 
 end
